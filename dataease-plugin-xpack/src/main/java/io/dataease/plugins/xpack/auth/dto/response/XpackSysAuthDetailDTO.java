f572946caa9f64a7a755953b27e4fd8b29389092
wangjiahao <1522128093@qq.com>
2021-06-03 14:48:52 +0800

    feat:优化权限查询逻辑

---
+++ dataease-plugin-xpack/src/main/java/io/dataease/plugins/xpack/auth/dto/response/XpackSysAuthDetailDTO.java
@@ -0,0 +1,21 @@
+package io.dataease.plugins.xpack.auth.dto.response;
+
+import io.dataease.plugins.common.annotation.PluginResultMap;
+import lombok.Data;
+
+/**
+ * Author: wangjiahao
+ * Date: 2021-06-03
+ * Description:
+ */
+@Data
+@PluginResultMap
+public class XpackSysAuthDetailDTO extends  XpackSysAuthDetail{
+    private String authSource;
+
+    private String authSourceType;
+
+    private String authTarget;
+
+    private String authTargetType;
+}
--- dataease-plugin-xpack/src/main/java/io/dataease/plugins/xpack/auth/service/AuthXpackService.java
+++ dataease-plugin-xpack/src/main/java/io/dataease/plugins/xpack/auth/service/AuthXpackService.java
@@ -4,6 +4,7 @@ import io.dataease.plugins.common.service.PluginMenuService;
 import io.dataease.plugins.xpack.auth.dto.request.XpackBaseTreeRequest;
 import io.dataease.plugins.xpack.auth.dto.request.XpackSysAuthRequest;
 import io.dataease.plugins.xpack.auth.dto.response.XpackSysAuthDetail;
+import io.dataease.plugins.xpack.auth.dto.response.XpackSysAuthDetailDTO;
 import io.dataease.plugins.xpack.auth.dto.response.XpackVAuthModelDTO;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +13,7 @@ public abstract class AuthXpackService extends PluginMenuService {
 
     public abstract List<XpackVAuthModelDTO> searchAuthModelTree(XpackBaseTreeRequest request, Long userId, Boolean isAdmin);
 
-    public abstract Map<String, List<XpackSysAuthDetail>> searchAuthDetails(XpackSysAuthRequest request);
+    public abstract Map<String, List<XpackSysAuthDetailDTO>> searchAuthDetails(XpackSysAuthRequest request);
 
     public abstract List<XpackSysAuthDetail> searchAuthDetailsModel(String authType);
 
