08e788fc8d291c302af4402833b37b57cdf89e85
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-02-10 12:10:33 +0800

    feat: 新增视图插件sdk

---
+++ dataease-plugin-view/src/main/java/io/dataease/plugins/view/entity/PluginViewType.java
@@ -0,0 +1,14 @@
+package io.dataease.plugins.view.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class PluginViewType implements Serializable {
+    private static final long serialVersionUID = -2715025716608100657L;
+
+    private String render;
+
+    private String name;
+}
