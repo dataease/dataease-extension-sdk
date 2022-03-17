3466103d4639e62e2d919419e0ce4393690f106c
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-03-17 17:13:43 +0800

    refactor: 重构视图插件

---
+++ dataease-plugin-view/src/main/java/io/dataease/plugins/view/entity/PluginViewSet.java
@@ -0,0 +1,13 @@
+package io.dataease.plugins.view.entity;
+
+import lombok.Data;
+
+@Data
+public class PluginViewSet {
+
+    private String type;
+
+    private String info;
+
+    private String dsType;
+}
