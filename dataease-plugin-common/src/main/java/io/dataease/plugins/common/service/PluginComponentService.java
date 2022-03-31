b22d93d7e2ffd41f6295f3b5b96bb11fa57df5a9
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-03-31 17:48:23 +0800

    refactor: 视图插件图标自定义

--- dataease-plugin-common/src/main/java/io/dataease/plugins/common/service/PluginComponentService.java
+++ dataease-plugin-common/src/main/java/io/dataease/plugins/common/service/PluginComponentService.java
@@ -1,19 +1,34 @@
 package io.dataease.plugins.common.service;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
+import io.dataease.plugins.common.dto.StaticResource;
 import org.apache.commons.lang3.StringUtils;
 
 public abstract class PluginComponentService {
 
+    private static final String SPLIT = ".";
+    private static final String DEFAULT_SUFFIX = "js";
+    private static final List emptyResult = new ArrayList();
+
     public abstract List<String> components();
+
+    public List<StaticResource> staticResources(){
+        return emptyResult;
+    }
 
     protected abstract InputStream readContent(String name);
 
     public InputStream vueResource(String name) {
-        if (StringUtils.isEmpty(name)) return null;
-        if (!name.endsWith(".js")) name += ".js";
+        return vueResource(name, DEFAULT_SUFFIX);
+    }
+
+    public InputStream vueResource(String name, String suffix) {
+        if (StringUtils.isEmpty(name) || StringUtils.isEmpty(suffix)) return null;
+        String lastName = SPLIT + suffix;
+        if (!name.endsWith(lastName)) name += lastName;
 
         return readContent(name);
     }
