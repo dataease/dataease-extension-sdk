189d600e43a58f78c974554867a816b7a0ccbcc4
taojinlong <jinlong@fit2cloud.com>
2022-07-25 17:25:04 +0800

    feat: 数据集列权限支持白名单

--- dataease-plugin-interface/src/main/java/io/dataease/plugins/xpack/auth/dto/request/DataSetColumnPermissionsDTO.java
+++ dataease-plugin-interface/src/main/java/io/dataease/plugins/xpack/auth/dto/request/DataSetColumnPermissionsDTO.java
@@ -9,4 +9,7 @@ public class DataSetColumnPermissionsDTO extends DatasetColumnPermissions{
     private String datasetName;
     private String authTargetName;
     private List<Long> authTargetIds;
+
+    @io.swagger.annotations.ApiModelProperty("白名单-用户（解析自whiteListUser参数）")
+    private java.util.List<io.dataease.plugins.common.base.domain.SysUser> whiteListUsers;
 }
