4d42335e1b1f225bc75fb5437fb4a4d30fc5dfdc
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-08-10 15:42:45 +0800

    feat: 主题设置接口适配新版UI

--- dataease-plugin-interface/src/main/java/io/dataease/plugins/xpack/theme/service/ThemeXpackService.java
+++ dataease-plugin-interface/src/main/java/io/dataease/plugins/xpack/theme/service/ThemeXpackService.java
@@ -2,18 +2,20 @@ package io.dataease.plugins.xpack.theme.service;
 
 import java.util.List;
 
+import io.dataease.plugins.xpack.theme.dto.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.dataease.plugins.common.service.PluginComponentService;
-import io.dataease.plugins.xpack.theme.dto.ThemeDto;
-import io.dataease.plugins.xpack.theme.dto.ThemeItem;
-import io.dataease.plugins.xpack.theme.dto.ThemeRequest;
 
 public abstract class ThemeXpackService extends PluginComponentService{
 
     public abstract List<ThemeDto> themes();
 
     public abstract void save(ThemeRequest request, MultipartFile file);
+
+    public abstract void addTheme(ThemeCreateRequest request);
+
+    public abstract void renameTheme(ThemeRenameRequest request);
 
     public abstract List<ThemeItem> queryItems(int id);
 
