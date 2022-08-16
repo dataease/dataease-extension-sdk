e42f8d0cee09565dbbdd79895650500f645901d4
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2022-08-16 13:50:20 +0800

    perf: 优化用户导入sdk

--- dataease-plugin-interface/src/main/java/io/dataease/plugins/xpack/user/dto/ExcelUserDTO.java
+++ dataease-plugin-interface/src/main/java/io/dataease/plugins/xpack/user/dto/ExcelUserDTO.java
@@ -2,27 +2,35 @@ package io.dataease.plugins.xpack.user.dto;
 
 import java.io.Serializable;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class ExcelUserDTO implements Serializable{
 
-    @ApiModelProperty("账号")
+    @ExcelProperty(index = 0, value = "ID（必填，文本）")
     private String username;
-    @ApiModelProperty("姓名")
+
+    @ExcelProperty(index = 1, value = "姓名（必填，文本）")
     private String nickName;
-    @ApiModelProperty("性别")
+
+    @ExcelProperty(index = 2, value = "性别（非必填，男或女）")
     private String gender;
-    @ApiModelProperty("邮箱")
+
+    @ExcelProperty(index = 3, value = "邮箱（必填，文本）")
     private String email;
-    @ApiModelProperty("电话")
+
+    @ExcelProperty(index = 4, value = "手机号（非必填，文本）")
     private String phone;
-    @ApiModelProperty("组织名称")
+
+    @ExcelProperty(index = 5, value = "组织名称（非必填，文本）")
     private String deptName;
-    @ApiModelProperty("角色名称集合")
+
+    @ExcelProperty(index = 6, value = "角色名称（非必填，文本）")
     private String roleNames;
-    @ApiModelProperty("状态")
+
+    @ExcelProperty(index = 7, value = "用户状态（必填，是否启用）")
     private String enable;
 
 
