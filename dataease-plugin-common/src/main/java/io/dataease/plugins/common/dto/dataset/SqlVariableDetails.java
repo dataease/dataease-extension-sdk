d0337d1f60d68745633b80c6af02be6e038e3d00
taojinlong <jinlong@fit2cloud.com>
2022-09-07 14:54:57 +0800

    feat: sql 变量支持时间类型

---
+++ dataease-plugin-common/src/main/java/io/dataease/plugins/common/dto/dataset/SqlVariableDetails.java
@@ -0,0 +1,15 @@
+package io.dataease.plugins.common.dto.dataset;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SqlVariableDetails {
+    private String variableName;
+    private String alias;
+    private List<String> type;
+    private String details;
+    private String defaultValue;
+
+}
