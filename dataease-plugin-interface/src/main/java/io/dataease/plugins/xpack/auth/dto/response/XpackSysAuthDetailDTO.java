5da989a6005bcdea3bf712aa962bfb74f8ce9364
wangjiahao <1522128093@qq.com>
2023-02-10 10:01:51 +0800

    refactor: Api文档整理

--- dataease-plugin-interface/src/main/java/io/dataease/plugins/xpack/auth/dto/response/XpackSysAuthDetailDTO.java
+++ dataease-plugin-interface/src/main/java/io/dataease/plugins/xpack/auth/dto/response/XpackSysAuthDetailDTO.java
@@ -1,6 +1,7 @@
 package io.dataease.plugins.xpack.auth.dto.response;
 
 import io.dataease.plugins.common.annotation.PluginResultMap;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -11,15 +12,16 @@ import lombok.Data;
 @Data
 @PluginResultMap
 public class XpackSysAuthDetailDTO extends  XpackSysAuthDetail{
+    @ApiModelProperty("授权源")
     private String authSource;
-
+    @ApiModelProperty("源类型")
     private String authSourceType;
-
+    @ApiModelProperty("授权目标")
     private String authTarget;
-
+    @ApiModelProperty("目标类型")
     private String authTargetType;
-
+    @ApiModelProperty("权限值")
     private Integer extPrivilegeValue;
-
+    @ApiModelProperty("目标名称")
     private String extTargetNames;
 }
